050b6bd4dfa0c0e882b4014607c1ff5cfffeeabb
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-01-13 17:00:45 -0500

    https://github.com/pmocal/jewelery-co-upwork/issues/1

---
+++ models/jewelery.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var JewelerySchema = new Schema(
+	{
+		reportId: {type: String, required: true},
+		customerInfo: {type: String, required: true},
+		description: {type: String, required: true},
+		stoneType: { type: String, required: true },
+		jewleryWeight: {type: Boolean, required: true},
+		totalStones: {type: Boolean, required: true},
+		comments: {type: Boolean, required: true},
+		serialNumber: {type: Boolean, required: true},
+		metalType: {type: Boolean, required: true},
+		caratWeight: {type: Boolean, required: true},
+		colorGrade: {type: Boolean, required: true},
+		clarityGrade: {type: Boolean, required: true},
+		estimatedRetailReplacementValue: {type: Boolean, required: true}
+	}
+)
+
+module.exports = mongoose.model('Jewelery', JewelerySchema);
---
+++ models/watch.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var WatchSchema = new Schema(
+	{
+		reportId: {type: String, required: true},
+		customerInfo: {type: String, required: true},
+		brand: {type: String, required: true},
+		referenceNumber: { type: String, required: true },
+		serialNumber: {type: Boolean, required: true},
+		model: {type: Boolean, required: true},
+		movement: {type: Boolean, required: true},
+		caseDiameter: {type: Boolean, required: true},
+		bezelMaterial: {type: Boolean, required: true},
+		dial: {type: Boolean, required: true},
+		braceletMaterial: {type: Boolean, required: true},
+		comments: {type: Boolean, required: true},
+		claspMaterial: {type: Boolean, required: true},
+		functions: {type: Boolean, required: true},
+		year: {type: Boolean, required: true},
+		condition: {type: Boolean, required: true},
+		estimatedRetailReplacementValue: {type: Boolean, required: true}
+	}
+)
+
+module.exports = mongoose.model('Jewelery', JewelerySchema);
